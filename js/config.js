f4b8b23c34ca3ad85a65a623df0aed4f555283e9
user <user>
2017-06-12 10:58:38 +0530

    dgfd

--- js/config.js
+++ js/config.js
@@ -111,32 +111,3 @@ function categories()
    });
 		
 }
-var push = PushNotification.init({
-            "android": {
-                    "senderID": "315537388956"
-            },
-            "ios": {
-                    "sound": true,
-                    "vibration": true,
-                    "badge": true
-            },
-            "windows": {}
-    });
-push.on('registration', function(data) {
-        console.log('registration event: ' + data.registrationId);
-		alert('registration event: ' + data.registrationId);
-
-        var oldRegId = localStorage.getItem('registrationId');
-        if (oldRegId !== data.registrationId) {
-            // Save new registration ID
-            localStorage.setItem('registrationId', data.registrationId);
-            // Post registrationId to your app server as the value has changed
-        }
-
-        /*var parentElement = document.getElementById('registration');
-        var listeningElement = parentElement.querySelector('.waiting');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');*/
-    });
